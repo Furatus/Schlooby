5270de6b36de1f5b83aea7a2636ec716ab7aef23
Furatus <furacoptere@gmail.com>
2026-08-07 12:49:59 +0200

    removed await in sqlite

--- sqlite.py
+++ sqlite.py
@@ -4,8 +4,8 @@ import os
 import sqlite3 as sqlite
 
 async def insert_keepalive_sqlite(userid, starttime, endtime):
-    conn = await sqlite.connect("schlooby.db")
+    conn = sqlite.connect("schlooby.db")
     c = conn.cursor()
-    await c.execute(f"INSERT INTO keepalives VALUES ({userid},{starttime},{endtime})")
+    c.execute(f"INSERT INTO keepalives VALUES ({userid},{starttime},{endtime})")
     conn.commit()
     conn.close()
